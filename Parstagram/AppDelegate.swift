fb417fd3ed83780bf28d6a048b0e2b7a4ac58e68
iman20004 <imanali4@gmail.com>
2021-03-12 15:25:39 -0500

    Started project and connected app to my parse server

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "k1B97MlPGdCTErcF0dSAbnTq1WYIhvkdOPXZZnZw"
+                    $0.clientKey = "eFapfudxYxhGSgnQ5MHi7Ry5OULsLHN7tWn3bgR2"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
+        
         return true
     }
 
