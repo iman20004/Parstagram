1734a911341f43417ecd99971104f4a0c7a40989
iman20004 <imanali4@gmail.com>
2021-03-18 22:58:01 -0400

    Stay logged in and logout

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -22,7 +22,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     @IBAction func onLogout(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let delegate = windowScene.delegate as? SceneDelegate
+        else {
+            return
+        }
+        delegate.window?.rootViewController = loginViewController
+        
     }
     
     
--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,9 +17,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name("login"), object: nil, queue: OperationQueue.main){
+            (Notification) in print("Login notification received")
+            
+            self.login()
+        }
+        
+        /*NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main){
+            (Notification) in print("Logout notification received")
+            
+            self.logOut()
+        }*/
+        
+        if PFUser.current() != nil {
+            login()
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    
+    func login (){
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        
+        window?.rootViewController = main.instantiateViewController(identifier: "FeedNavigationController")
+    }
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
